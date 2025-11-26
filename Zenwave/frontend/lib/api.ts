2b2fdf26b0bad01d2f2d8e857e58611787fdc1ac
Yamil Pedroso <yamirovinci@gmail.com>
2025-11-26 13:14:18 +0100

    Add zenwave-api to the client

--- Zenwave/frontend/lib/api.ts
+++ Zenwave/frontend/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/useAuthStore";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3010/api",
+  baseURL: "https://zenwave-api.yampe.dev",
 });
 
 // Attach the token before every request
